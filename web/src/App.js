5a4bd9fa5e93b67bba6866c1e4c86d3f66ca1040
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2020-09-01 19:30:26 +0100

    criacao do front-end web

---
+++ web/src/App.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
+import DevItem from './components/DevItem';
+import DevForm from './components/DevForm';
+
+
+function App() {
+  const [devs, setDevs] = useState([]);
+  
+
+  useEffect(()=>{
+    async function handleLoadDevs(){
+      const response = await api.get('/devs');
+      setDevs(response.data);
+    }
+    handleLoadDevs();
+  },[]);
+
+  async function handleAddDev(data) {
+    
+    const response = await api.post('/devs', data);
+
+    setDevs([...devs, response.data]);
+  }
+
+
+  return (
+    <>
+      <div id="app">
+          <aside>
+            <strong>Cadastrar</strong>
+            <DevForm onSubmit={handleAddDev}/>
+          </aside>
+          <main>
+            <ul>
+              {
+                devs.map(dev => (
+                  <DevItem key={dev._id} dev={ dev }/> 
+                ))
+              }           
+            </ul>
+          </main>
+      </div>
+    </>
+  );
+}
+
+export default App;
